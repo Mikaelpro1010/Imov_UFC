869b2cfe976750cc26d589a93f76d360a7f886fb
Mikaelpro1010 <mkpro10@alu.ufc.br>
2023-12-03 20:45:28 -0300

    Aidiconando novas colunas na tabela de apartamentos do banco de dados

--- app_imov_ufc/models.py
+++ app_imov_ufc/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Apartment(models.Model):
     id_apartment = models.AutoField(primary_key=True)
     preco = models.FloatField()
+    avaliacao = models.FloatField(default=0.0)
+    quant_curtidas = models.IntegerField(default=0)
     endereco = models.TextField(max_length=255)
     descricao = models.TextField(max_length=300)
     
